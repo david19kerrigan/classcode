28f6d20b14c2ad01722651b16f2d0376d94363b0
zamansky <zamansky@gmail.com>
2014-09-17 07:28:31 -0400

    Z

---
+++ shared/Greeter/Driver.java
@@ -0,0 +1,24 @@
+
+
+public class Driver {
+
+    public static void main(String[] args){
+	Greeter g1 = new Greeter();
+	Greeter g2 = new Greeter();
+
+	g1.greet();
+	g2.greet();
+
+	/*
+	  1. Type in the greeter and driver and test them
+	  2. Uncoment the line specified in greeter, run - what happens and why
+	  3. See if you can figure out how to change the instance variable 
+	     greeting in g1 by adding a line of code in driver 
+	     (if you can, help your neighbors).
+	  4. Run again - are g1 and g2 greeting you differntly now? 
+	     Why or why not?
+	  5. What if we make the instance variable greeting private.
+	     Does it change what you  did for questions 3 and 4?
+	*/
+    }
+}
---
+++ shared/Greeter/Greeter.java
@@ -0,0 +1,14 @@
+
+public class Greeter {
+
+    public String greeting = new String("Hello world!");
+    public void greet() {
+	// uncomment the following line for question 2
+	//String greeting = new String("Sup!");
+	System.out.println(greeting);
+    }
+
+    public void ungreet() {
+	System.out.println("I'm out!");
+    }
+}
