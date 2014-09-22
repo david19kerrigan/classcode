5e741e76ba6c2832cce482ebc26eccea0724eb32
Mike Zamansky <zamansky@gmail.com>
2014-09-22 08:11:04 -0400

    Z

--- shared/StringStuff/StringStuff.java
+++ shared/StringStuff/StringStuff.java
@@ -9,10 +9,10 @@ public class StringStuff {
 	System.out.println("s1==s2: "+(s1 == s2));
 	System.out.println("s1==s3: "+(s1 == s3));
 	System.out.println("s1==s4: "+(s1 == s4));
-	System.out.println("s2==s3: "+(s2 == s4));
+	System.out.println("s2==s3: "+(s2 == s3));
 	System.out.println("s2==s4: "+(s2 == s4));
 	System.out.println("s3==s4: "+(s3 == s4));
-	
+
     }
 
 }
