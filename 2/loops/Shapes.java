258e96698bc7b95556a305f51158f4717c15f21a
Mike Zamansky <zamansky@gmail.com>
2014-10-02 09:14:59 -0400

    Z

--- 2/loops/Shapes.java
+++ 2/loops/Shapes.java
@@ -15,4 +15,45 @@ public class Shapes {
 
 	return s;
     }
+
+    public String tri1(int h){
+	String s = "";
+
+	/*
+	  int i=0;
+	  while (i<h){
+	  
+	  i++;
+	  }
+	 */
+
+	int i,j;
+	for (i=0;i<h;i++){
+	    for (j=0;j<=i;j++) {
+		s=s+"*";
+	    }
+	    s=s+"\n";
+	}
+	return s;
+    }
+
+    public String tri2(int height){
+	int h;
+	int i;
+        String s="";
+	for (h=1;h<=height;h++){
+	    /* add the next space line */
+	    for (i=0;i<height-h;i++){
+		s = s + ".";
+	    }
+
+	    /* add the next triangle line */
+	    for (i=0;i<h;i++){
+		s=s+"*";
+	    }
+	    s=s+"\n";
+	}
+	return s;
+    }
+
 }
