f91357c0e187e2c53822b19e1c525661eacc4b8f
Mike Zamansky <zamansky@gmail.com>
2014-10-03 08:41:20 -0400

    Z

--- 1/loops/Driver.java
+++ 1/loops/Driver.java
@@ -2,11 +2,14 @@
 public class Driver {
     public static void  main(String[] args){
 	Shapes s =  new Shapes();
-	System.out.println(s.box(5,4));
-	System.out.println(s.box(4,5));
-	System.out.println();
-	System.out.println(s.tri1(5));
-	System.out.println();
-	System.out.println(s.tri2(5));
+	// System.out.println(s.box(5,4));
+	// System.out.println(s.box(4,5));
+	// System.out.println();
+	// System.out.println(s.tri1(5));
+	// System.out.println();
+	// System.out.println(s.tri2(5));
+	System.out.println(s.tri3(4));
+	System.out.println(s.diamond(5));
+	System.out.println(s.line("$",10));
     }
 }
--- 1/loops/Shapes.java
+++ 1/loops/Shapes.java
@@ -2,6 +2,14 @@
 
 public class Shapes {
 
+
+    public String line(String c, int n){
+	String s = "";
+	for (int i=0;i<n;i++){
+	    s=s+c;
+	}
+	return s;
+    }
     public String box(int r, int c){
 	String s= "";
 	int row = 0;
@@ -66,5 +74,35 @@ public class Shapes {
 
     }
 	
-    
+    public String tri3(int height){
+	int r;
+	String s="";
+	for (r=0;r<height;r++){
+	    /*
+	    s = s + line(".",height-r-1);
+	    s = s + line("*",2*r+1);
+	    s=s+"\n";
+	    */
+	    s=s+line(".",height-r-1)+
+		line("*",2*r+1)+
+		"\n";
+		
+	}
+	return s;
+    }
+
+    public String diamond(int h){
+	String s = tri3((h+1)/2);
+	int r;
+	for (r=(h- 1)/2-1;r>=0;r--){
+	    for (int i=0;i<h-r-3;i++){
+		s=s+".";
+	    }
+	    for (int i=0;i<2*r+1;i++){
+		s=s+"*";
+	    }
+	    s=s+"\n";
+	}
+	return s;
+    }
 }
