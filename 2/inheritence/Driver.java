f41d445a20f2b8af0b2fd34328e3602b48331854
Mike Zamansky <zamansky@gmail.com>
2014-10-06 09:25:26 -0400

    Z

---
+++ 2/inheritence/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static void main(String[] args){
+	Character c = new Character();
+	System.out.println(c.getHealth());
+	Warrior w = new Warrior();
+	System.out.println(w.getHealth());
+    }
+
+}
