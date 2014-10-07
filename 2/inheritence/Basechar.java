d6aa90e5094a8f3ea4eae7d8d84d96d645137a84
Mike Zamansky <zamansky@gmail.com>
2014-10-07 09:23:17 -0400

    Z

--- 2/inheritence/Basechar.java
+++ 2/inheritence/Basechar.java
@@ -1,4 +1,5 @@
-public class Character {
+/* superclass for warrior (base class) and Mage */
+public class Basechar {
     private int health=20;
     public int getHealth() {
 	return health;
--- 2/inheritence/Warrior.java
+++ 2/inheritence/Warrior.java
@@ -1,3 +1,9 @@
-public class Warrior extends Character {
-
+/* This is a subclass of Basechar */
+public class Warrior extends Basechar {
+    // private int health= 50000;
+    private int healthboost = 5;
+    public int getHealth() {
+	return healthboost + super.getHealth();
+    }
+    
 }
