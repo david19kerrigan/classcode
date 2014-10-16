4517d9a83c797aa21d85bef62c344c5e157589e8
Mike Zamansky <zamansky@gmail.com>
2014-10-16 09:11:21 -0400

    Z

--- shared/test2/Driver.java
+++ shared/test2/Driver.java
@@ -3,6 +3,26 @@ public class Driver{
 	Complex c1 = new Complex(3,4);
 	System.out.println(c1);
 	System.out.println(c1.norm());
+	String s = ""+12345 ; // points s to the String "12345"
+	int i = Integer.parseInt("12345");//  converts String to int
+	double d = Double.parseDouble("123.45");// converts String to double
+
+	System.out.println("You are attacked by the demon");
+
+	try {
+	    Thread.sleep(2000);
+	} catch (Exception e){}
+
+	System.out.println("It hits");
+
+	try {
+	    Thread.sleep(4000);
+	} catch (Exception e){}
+	
+	System.out.println("And does a huge amount of damage");
+	
+					       
+	    
     }
 
 }
