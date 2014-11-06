3520dff7c042ffc772735b9ab530a6092c8ecf13
Mike Zamansky <zamansky@gmail.com>
2014-11-06 09:19:13 -0500

    Added fastmode

--- 2/mode/Mode.java
+++ 2/mode/Mode.java
@@ -64,22 +64,73 @@ public class Mode {
 				}
 				return count;
 		}
+
+		public int maxVal(int[] a){
+				int maxSoFar = a[0];
+				for (int i = 0; i < a.length; i++) {
+						if (a[i] > maxSoFar)
+								maxSoFar = a[i];
+				}
+				return maxSoFar;
+		}
+		
+		public int maxIndex(int[] a){
+				int maxi = 0;
+				for (int i = 0; i < a.length; i++) {
+						if (a[i] > a[maxi])
+								maxi = i;
+				}
+				return maxi;
+		}
+		
+		public int fastmode(){
+				int mv = maxVal(a);
+				int[] tallies = new int[mv+1];
+
+				// Java initialises the array to all 0s so we don't need:
+				/*
+				for (int i = 0; i < tallies.length; i++) {
+						tallies[i]=0;
+				}
+				*/
+				for (int i = 0; i < a.length; i++) {
+						tallies[ a[i] ] += 1;
+				}
+				
+				int mi = maxIndex(tallies);
+				
+				
+				/* just for testing purposes */
+				System.out.println("Modecount: "+tallies[mi]);
+
+				return mi;
+				
+		}
+		
 		/*----------- main --------*/
 
 		public static void main(String[] args) {
 				int arraylength=20,
 						maxvalue=20;
 
-				if (args.length > 0) {
-						arraylength = Integer.parseInt(args[0]);
+				if (args.length == 0){
+						System.out.println("First argument must be f or s");
+						System.exit(0);
 				}
+				String choice = args[0];
 				if (args.length > 1) {
-						maxvalue = Integer.parseInt(args[1]);
+						arraylength = Integer.parseInt(args[1]);
 				}
+				if (args.length > 2) {
+						maxvalue = Integer.parseInt(args[2]);
+				}
+
 				
 				Mode m = new Mode(arraylength,maxvalue);
-				// System.out.println(m);
-				System.out.println("Mode value: "+m.mode());
+				if (choice.equals("s"))
+						System.out.println("Mode value: "+m.mode());
+				else
+						System.out.println("Fastmode value: "+m.fastmode());
 		}
 
 }
