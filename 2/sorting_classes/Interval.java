b0de29d75c6e9b7a0cdc37ac752e6f2fd2773bfb
Mike Zamansky <zamansky@gmail.com>
2014-12-10 09:15:46 -0500

    Z

--- 2/sorting_classes/Interval.java
+++ 2/sorting_classes/Interval.java
@@ -1,7 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-class Interval {
+
+/*
+
+interface name {
+  method1_signature;
+	method2_signature;
+	etc
+}
+
+method signature is:  public/private return_type name(params);
+
+For example, the built in Java Comparable interface looks something
+like this:
+
+interface Comparable { 
+  public int compareTo(Object other);
+}
+
+To agree to implement an interface use "implements"
+ex:
+
+class myClass implements Comparable{
+ // class definitions here
+}
+
+A class can only extend one class but it can implement as many
+interfaces as it wants (separated by ,)
+ */
+
+
+
+
+
+class Interval implements Comparable {
 
 		private int low, high;
 		private static Random r = new Random();
@@ -29,6 +62,20 @@ class Interval {
 				return s;
 		}
 
+		public int compareTo(Object other){
+				/* create a local variable and cast and assign the parameter */
+				Interval o = (Interval)other;
+				/*
+					if (this.low == o.low){
+						return this.high - o.high;
+				} else {
+						return this.low - o.low;
+				}
+
+				*/
+				return (this.high - this.low) - (o.high - o.low);
+		}
+		
 		public static void printStuff() {
 				System.out.println("Stuff");
 		}
@@ -49,7 +96,22 @@ class Interval {
 				for (int i = 0; i < a.length; i++) {
 						a[i] = new Interval();
 				}
-
 				System.out.println(Arrays.toString(a));
+
+				String[] sa = {"frog","toad","igauna","komodo dragon","bearded lizard"};
+				System.out.println(Arrays.toString(sa));
+				Arrays.sort(sa);
+				System.out.println(Arrays.toString(sa));
+
+				int[] ia = {20,1,55,100,33,201,24,17,78};
+				System.out.println(Arrays.toString(ia));
+				Arrays.sort(ia);
+				System.out.println(Arrays.toString(ia));
+				
+				System.out.println(Arrays.toString(a));
+				Arrays.sort(a);
+				System.out.println(Arrays.toString(a));
+				
+				
 		}
 }
