66f1ead8ed262853e773ef62a707bf67be79f0db
Mike Zamansky <zamansky@gmail.com>
2015-02-08 15:12:58 -0500

    Z

--- shared/mazebase/Maze.java
+++ shared/mazebase/Maze.java
@@ -9,48 +9,48 @@ public class Maze
 
     public Maze() 
     {
-				maxX=40;
-				maxY=20;
-				board = new char[maxX][maxY];
-				
-				try {
-						
-						Scanner sc = new Scanner(new File("maze.dat"));
-						int j=0;
-						while (sc.hasNext())
-								{
-										String line = sc.nextLine();
-										for (int i=0;i<maxX;i++)
-												{
-														board[i][j] = line.charAt(i);
-												}
-										j++;
-								}
-				}
-				catch (Exception e)
-						{
-						}
-				
+    maxX=40;
+    maxY=20;
+    board = new char[maxX][maxY];
+    
+    try {
+      
+      Scanner sc = new Scanner(new File("maze.dat"));
+      int j=0;
+      while (sc.hasNext())
+        {
+          String line = sc.nextLine();
+          for (int i=0;i<maxX;i++)
+            {
+              board[i][j] = line.charAt(i);
+            }
+          j++;
+        }
+    }
+    catch (Exception e)
+      {
+      }
+    
     }
 
     public String toString()
     {
-				String s = "[2J\n";
-				for (int y=0;y<maxY;y++)
-						{
-								for (int x=0;x<maxX;x++)
-										s = s +board[x][y];
-								s=s+"\n";
-	    }
-				
-				return s;
+    String s = "[2J\n";
+    for (int y=0;y<maxY;y++)
+      {
+        for (int x=0;x<maxX;x++)
+          s = s +board[x][y];
+        s=s+"\n";
+      }
+    
+    return s;
     }
 
-		public static void main(String[] args){
-				Maze m = new Maze();
-				System.out.println(m);
-				
-		}
-		
+  public static void main(String[] args){
+    Maze m = new Maze();
+    System.out.println(m);
+    
+  }
+  
 }
 
