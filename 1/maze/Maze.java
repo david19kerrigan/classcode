6c029d84867ca7f35e0b482da5b773ecaeb0ab06
Mike Zamansky <zamansky@gmail.com>
2015-02-10 09:21:27 -0500

    Z

--- 1/maze/Maze.java
+++ 1/maze/Maze.java
@@ -6,84 +6,89 @@ public class Maze
     private char[][] board;
     private int maxX;
     private int maxY;
-		private char wall=' ';
-		private char exit='$';
+
 		private char path='#';
+		private char wall=' ';
 		private char me='z';
-		private char visited='.';
+		private char exit='$';
+		private char visited = '.';
+		private boolean solved = false;
+		
+		public void delay(int n){
+				try {
+						Thread.sleep(n);
+				} catch (Exception e) {}
+		}
+		
     public Maze() 
     {
 				maxX=40;
 				maxY=20;
 				board = new char[maxX][maxY];
-				int i=0,j=0;
-    try {
-				Scanner sc = new Scanner(new File("maze.dat"));
-				j=0;
-				while (sc.hasNext())
-						{
-								String line = sc.nextLine();
-								for (i=0;i<maxX;i++)
-										{
-												board[i][j] = line.charAt(i);
-										}
-								j++;
-						}
-    }
-    catch (Exception e)
-				{
+				
+				try {
+						
+						Scanner sc = new Scanner(new File("maze.dat"));
+						int j=0;
+						while (sc.hasNext())
+								{
+										String line = sc.nextLine();
+										for (int i=0;i<maxX;i++)
+												{
+														board[i][j] = line.charAt(i);
+												}
+										j++;
+								}
 				}
-    
+				catch (Exception e)
+						{
+						}
+				
     }
 		
     public String toString()
     {
-
-				String ANSI_CLS = "\u001b[2J";
-				String s = ANSI_CLS;
+				String s = "[2J\n";
+				
 				for (int y=0;y<maxY;y++)
 						{
 								for (int x=0;x<maxX;x++)
 										s = s +board[x][y];
 								s=s+"\n";
 						}
-				
 				return s;
     }
-		
 
+		/*
+			solved - instance variable to indicate we're done
+			
+		 */
 		public void solve(int x, int y){
+				if (board[x][y]==wall ||
+						board[x][y]==me ||
+						board[x][y]==visited ||
+						solved){
+						return;
+				}
 
-				try {
-						Thread.sleep(50);
-				} catch (Exception e){}
 				if (board[x][y]==exit){
 						System.out.println(this);
-						System.exit(0);
+						solved = true;
 				}
-				if (board[x][y] != path)
-						return;
-
+				delay(100);
+				System.out.println(this);
 				board[x][y]=me;
-				System.out.print(this);
-
-				System.out.print(x+","+y);
-				
 				solve(x+1,y);
 				solve(x-1,y);
 				solve(x,y+1);
 				solve(x,y-1);
 				board[x][y]=visited;
-				
-		}
-
-
-		public static void main(String[] args){
-				Maze m = new Maze();
-				System.out.print(m);
-				m.solve(1,1);
-				
 		}
 		
+		public static void main(String[] args){
+				Maze m = new Maze();
+				System.out.println(m);
+				m.solve(1,1);
+		}
 }
 
