6fb7be928ba6edbf0c7a598ebdcb8436b2f6b20f
Mike Zamansky <zamansky@gmail.com>
2015-02-10 08:40:15 -0500

    Z

--- 1/maze/Maze.java
+++ 1/maze/Maze.java
@@ -6,20 +6,24 @@ public class Maze
     private char[][] board;
     private int maxX;
     private int maxY;
-
+		private char wall=' ';
+		private char exit='$';
+		private char path='#';
+		private char me='z';
+		private char visited='.';
     public Maze() 
     {
 				maxX=40;
 				maxY=20;
 				board = new char[maxX][maxY];
-    
+				int i=0,j=0;
     try {
 				Scanner sc = new Scanner(new File("maze.dat"));
-				int j=0;
+				j=0;
 				while (sc.hasNext())
 						{
 								String line = sc.nextLine();
-								for (int i=0;i<maxX;i++)
+								for (i=0;i<maxX;i++)
 										{
 												board[i][j] = line.charAt(i);
 										}
@@ -34,7 +38,9 @@ public class Maze
 		
     public String toString()
     {
-				String s = "[2J\n";
+
+				String ANSI_CLS = "\u001b[2J";
+				String s = ANSI_CLS;
 				for (int y=0;y<maxY;y++)
 						{
 								for (int x=0;x<maxX;x++)
@@ -45,10 +51,37 @@ public class Maze
 				return s;
     }
 		
+
+		public void solve(int x, int y){
+
+				try {
+						Thread.sleep(50);
+				} catch (Exception e){}
+				if (board[x][y]==exit){
+						System.out.println(this);
+						System.exit(0);
+				}
+				if (board[x][y] != path)
+						return;
+
+				board[x][y]=me;
+				System.out.print(this);
+
+				System.out.print(x+","+y);
+				
+				solve(x+1,y);
+				solve(x-1,y);
+				solve(x,y+1);
+				solve(x,y-1);
+				board[x][y]=visited;
+				
+		}
+
+
 		public static void main(String[] args){
 				Maze m = new Maze();
-				System.out.println(m);
-				System.out.println(m);
+				System.out.print(m);
+				m.solve(1,1);
 				
 		}
 		
