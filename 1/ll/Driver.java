62e953c6b36995daa959c4e7a4a3e7be7694be13
Mike Zamansky <zamansky@gmail.com>
2015-03-12 08:22:34 -0400

    Z

--- 1/ll/Driver.java
+++ 1/ll/Driver.java
@@ -9,16 +9,37 @@ public class Driver{
 				
 				n.setNext(n2);
 				
-				System.out.println(n.getNext().getData());
-				System.out.println(n.getNext());
+				//System.out.println(n.getNext().getData());
+				//System.out.println(n.getNext());
 
 				n.getNext().setData("pickle");
-				System.out.println(n2);
-				System.out.println(n.getNext().getData());
+				//System.out.println(n2);
+				//System.out.println(n.getNext().getData());
 
 				n2.setData("watermellon");
-System.out.println(n.getNext().getData());
-				
+				//System.out.println(n.getNext().getData());
+
+				n2.setNext(new Node("abc"));
+				//System.out.println(n.getNext().getNext().getData());
+
+				n.getNext().getNext().setNext(new Node("yabba dabba doo"));
+
+				Node tmp = new Node("start");
+				tmp.setNext(n);
+				n = tmp;
+				//System.out.println(tmp);
+				//System.out.println(n);
+				//System.out.println(n.getNext());
+
+				tmp = n;
+				while (tmp != null){
+						System.out.println(tmp);
+						tmp = tmp.getNext();
+				}
+				System.out.println();
+				for (tmp = n; tmp != null; tmp = tmp.getNext()){
+						System.out.println(tmp);
+				}
 		}
 		
 
